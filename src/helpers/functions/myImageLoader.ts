d9c680ef977df6f20d7c2ecfe6a04518e9dde044
Ole Larsen <ole.larssen777@gmail.com>
2023-08-02 02:17:38 +0530

    add default dpr parameter

--- src/helpers/functions/myImageLoader.ts
+++ src/helpers/functions/myImageLoader.ts
@@ -1,5 +1,5 @@
 import { ImageLoaderProps } from "next/image"
 
 export default function myImageLoader({ src, width, quality }: ImageLoaderProps) {
-  return `${src}&w=${width}&dpr=${quality}`
+  return `${src}&w=${width}&dpr=${quality ? quality : 1}`
 }
