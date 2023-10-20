17645d1702fcb96a83e9f98fb4da9a57216f8316
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-10-20 09:58:51 +0300

    --fix

--- src/app/(users)/(profile)/(main)/notifications/page.tsx
+++ src/app/(users)/(profile)/(main)/notifications/page.tsx
@@ -1,19 +1,20 @@
 "use client"
 
-import { useQuery } from "react-query"
-import { isMobile } from "react-device-detect"
+// import { useQuery } from "react-query"
+// import { isMobile } from "react-device-detect"
 
-import { useAuth } from "@/store/hooks"
-import { serviceLogs } from "@/services/logs"
+// import { useAuth } from "@/store/hooks"
+// import { serviceLogs } from "@/services/logs"
 
-import styles from "./style.module.scss"
+// import styles from "./style.module.scss"
 
 export default function Notifications() {
-    const { userId } = useAuth()
-    const { data } = useQuery({
-        queryFn: serviceLogs.get,
-        queryKey: ["logs"],
-    })
+    // const { userId } = useAuth()
+    // const { data } = useQuery({
+    //     queryFn: serviceLogs.get,
+    //     queryKey: ["logs"],
+    // })
 
-    return <ul className={styles.wrapper}></ul>
+    return <></>
+    // return <ul className={styles.wrapper}></ul>
 }
