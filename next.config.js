fc43fe88491127a12cc5b8682ac548ac0ab52997
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-12-09 13:32:16 +0300

    добавлена метрика и линк ссылка на профиль

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
     swcMinify: true,
     env: {
         NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
-        NEXT_PUBLIC_AUTO_VERIFICATION:
-            process.env.NEXT_PUBLIC_AUTO_VERIFICATION,
+        NEXT_PUBLIC_AUTO_VERIFICATION: process.env.NEXT_PUBLIC_AUTO_VERIFICATION,
         NEXT_PUBLIC_WEB_SOCKET: process.env.NEXT_PUBLIC_WEB_SOCKET,
         NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
         NEXT_PUBLIC_API_KEY_YANDEX: process.env.NEXT_PUBLIC_API_KEY_YANDEX,
