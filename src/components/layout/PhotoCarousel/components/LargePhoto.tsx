560cdd386c5d1878598ca60fbfde234d96ed610e
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-12-22 16:14:29 +0300

    пофикшены проблемы с чатом и лаяут оффера

--- src/components/layout/PhotoCarousel/components/LargePhoto.tsx
+++ src/components/layout/PhotoCarousel/components/LargePhoto.tsx
@@ -10,15 +10,21 @@ import styles from "./styles/style.module.scss"
 
 export function LargePhoto() {
     const currentPhoto = useVisiblePhotosCarousel(({ currentPhoto }) => currentPhoto)
+    const photos = useVisiblePhotosCarousel(({ photos }) => photos)
 
     const handlers = useSwipeable({
         onSwipedLeft: setNextPhotoCarousel,
         onSwipedRight: setPrevPhotoCarousel,
     })
 
-    return (
-        <div className={styles.containerLargePhoto} {...handlers}>
-            <NextImageMotion src={currentPhoto?.url!} onClick={setNextPhotoCarousel} alt="offer-image" height={590} width={960} />
+    return photos?.map((item) => (
+        <div
+            className={styles.containerLargePhoto}
+            {...handlers}
+            key={`::${item?.id}::current::photo::`}
+            data-current={item?.id === currentPhoto?.id}
+        >
+            <NextImageMotion src={item?.url!} onClick={setNextPhotoCarousel} alt="offer-image" height={590} width={960} />
         </div>
-    )
+    ))
 }
