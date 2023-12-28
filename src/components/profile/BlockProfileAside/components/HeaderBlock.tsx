38ee3d32a8ca7af2fed354bde147a21c4f9e4e46
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-12-29 02:04:00 +0300

    изменеия в подгрузке профиля

--- src/components/profile/BlockProfileAside/components/HeaderBlock.tsx
+++ src/components/profile/BlockProfileAside/components/HeaderBlock.tsx
@@ -1,29 +1,39 @@
 import dayjs from "dayjs"
 import Image from "next/image"
+import { useQuery } from "@tanstack/react-query"
 
 import type { THeaderBlock } from "./types/types"
 
 import { GeoTagging } from "@/components/common/GeoTagging"
 import { ImageStatic, NextImageMotion } from "@/components/common"
 
-import { useAddress } from "@/helpers"
 import { useAuth } from "@/store/hooks"
+import { serviceUsers } from "@/services/users"
 
 import styles from "./styles/style.module.scss"
 
 export const HeaderBlock: THeaderBlock = () => {
-    const user = useAuth(({ user }) => user)
-    const imageProfile = useAuth(({ imageProfile }) => imageProfile)
-    const createdUser = useAuth(({ createdUser }) => createdUser)
-    const { addressMain } = useAddress()
+    const userId = useAuth(({ userId }) => userId)
+
+    const { data } = useQuery({
+        queryFn: () => serviceUsers.getId(userId!),
+        queryKey: ["user", userId],
+        enabled: !!userId,
+    })
+
+    const addressMain = data?.res?.addresses?.find((item) => item?.addressType === "main") || ""
 
     return (
         <header className={styles.containerHeader}>
             <div className={styles.avatar}>
-                {imageProfile?.attributes?.url ? (
+                {data?.res?.profile?.image?.attributes?.url ? (
                     <NextImageMotion
                         className={styles.photo}
-                        src={imageProfile?.attributes?.url! ? imageProfile?.attributes?.url! : "/png/default_avatar.png"}
+                        src={
+                            data?.res?.profile?.image?.attributes?.url!
+                                ? data?.res?.profile?.image?.attributes?.url!
+                                : "/png/default_avatar.png"
+                        }
                         alt="avatar"
                         width={94}
                         height={94}
@@ -37,10 +47,10 @@ export const HeaderBlock: THeaderBlock = () => {
             </div>
             <section className={styles.title}>
                 <h4>
-                    {user?.firstName || "Имя"} {user?.lastName || "Фамилия"}
+                    {data?.res?.profile?.firstName || "Имя"} {data?.res?.profile?.lastName || "Фамилия"}
                 </h4>
                 {addressMain ? <GeoTagging size={16} fontSize={14} location={addressMain?.additional} /> : null}
-                {createdUser ? <p data-start>На Sheira с {dayjs(createdUser!).format("DD.MM.YYYY")}</p> : null}
+                {data?.res?.created ? <p data-start>На Sheira с {dayjs(data?.res?.created!).format("DD.MM.YYYY")}</p> : null}
             </section>
         </header>
     )
--- src/components/profile/MyProfilePage/components/M_ContainerAboutProfile.tsx
+++ src/components/profile/MyProfilePage/components/M_ContainerAboutProfile.tsx
@@ -2,33 +2,42 @@
 
 import dayjs from "dayjs"
 import Image from "next/image"
+import { useQuery } from "@tanstack/react-query"
 
 import { Button } from "@/components/common"
 import { GeoTagging } from "@/components/common/GeoTagging"
 import { ImageStatic, NextImageMotion } from "@/components/common/Image"
 
-import { useAddress, useOut } from "@/helpers"
+import { useOut } from "@/helpers"
+import { serviceUsers } from "@/services/users"
 import { useAuth, useUpdateProfile, dispatchNewServicesBanner } from "@/store/hooks"
 
 import styles from "./styles/style.module.scss"
 
 export const M_ContainerAboutProfile = () => {
-    const { addressMain } = useAddress()
     const setVisible = useUpdateProfile(({ setVisible }) => setVisible)
     const { out } = useOut()
-    const user = useAuth(({ user }) => user)
-    const imageProfile = useAuth(({ imageProfile }) => imageProfile)
-    const createdUser = useAuth(({ createdUser }) => createdUser)
+    const userId = useAuth(({ userId }) => userId)
+
+    const { data } = useQuery({
+        queryFn: () => serviceUsers.getId(userId!),
+        queryKey: ["user", userId],
+        enabled: !!userId,
+    })
+
+    const profile = data?.res?.profile
+
+    const addressMain = data?.res?.addresses?.find((item) => item?.addressType === "main") || null
 
     return (
         <section className={styles.containerMAboutProfile}>
             <div className={styles.blockAboutPhoto}>
                 <div className={styles.blockPhotoAch}>
                     <div className={styles.avatar}>
-                        {imageProfile?.attributes?.url ? (
+                        {profile?.image?.attributes?.url ? (
                             <NextImageMotion
                                 className={styles.photo}
-                                src={imageProfile?.attributes?.url}
+                                src={profile?.image?.attributes?.url}
                                 alt="avatar"
                                 width={94}
                                 height={94}
@@ -36,18 +45,18 @@ export const M_ContainerAboutProfile = () => {
                         ) : (
                             <ImageStatic src="/png/default_avatar.png" alt="avatar" width={400} height={400} className={styles.photo} />
                         )}
-                        {user ? (
+                        {profile ? (
                             <Image className={styles.verified} src="/svg/verified-tick.svg" alt="tick" width={24} height={24} unoptimized />
                         ) : null}
                     </div>
                 </div>
                 <div className={styles.aboutBlock}>
                     <h4>
-                        {user?.firstName} {user?.lastName}
+                        {profile?.firstName} {profile?.lastName}
                     </h4>
                     {addressMain ? <GeoTagging size={16} fontSize={12} location={addressMain?.additional} /> : null}
-                    <p className={styles.date}>На Sheira с {createdUser ? dayjs(createdUser).format("DD.MM.YYYY") : null}</p>
-                    <p className={styles.about}>{user?.about}</p>
+                    <p className={styles.date}>На Sheira с {data?.res?.created ? dayjs(data?.res?.created).format("DD.MM.YYYY") : null}</p>
+                    <p className={styles.about}>{profile?.about}</p>
                 </div>
             </div>
             <div className={styles.buttons}>
