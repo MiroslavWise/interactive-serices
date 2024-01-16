d0c91c9309e66f473362b2486369e5b142222a7c
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2024-01-16 22:32:15 +0300

    fix выплывающего тоастера

--- src/context/Containers.tsx
+++ src/context/Containers.tsx
@@ -69,7 +69,7 @@ export const Containers = () => {
                     <AboutSheiraPopup />
                 </>
             )}
-            <ToastContainer limit={3} />
+            <ToastContainer />
             {isMobile && (
                 <>
                     <FooterMenu />
