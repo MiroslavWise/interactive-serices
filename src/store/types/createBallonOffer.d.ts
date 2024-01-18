028ded84eeeb205aedb09710ca5d6a6ba52511dc
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2024-01-18 20:38:12 +0300

    добавлены офферы модалки из чата

--- src/store/types/createBallonOffer.d.ts
+++ src/store/types/createBallonOffer.d.ts
@@ -1,12 +1,13 @@
 import type { IResponseOffers } from "@/services/offers/types"
-import { IGetProfileIdResponse } from "@/services/profile/types/profileService"
+import type { ISmallDataOfferBarter } from "@/services/barters/types"
+import type { IGetProfileIdResponse } from "@/services/profile/types/profileService"
 
 export interface IStateBallonOffer {
     visible: boolean
-    offer?: IResponseOffers
+    offer?: ISmallDataOfferBarter | IResponseOffers
 }
 
 export interface IDispatchBallonOffer {
     visible: boolean
-    offer?: IResponseOffers
+    offer?: ISmallDataOfferBarter | IResponseOffers
 }
