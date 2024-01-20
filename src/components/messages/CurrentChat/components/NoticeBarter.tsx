52bf7d488934632c91c3a9a0fb306720bca118cd
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2024-01-20 12:50:29 +0300

    добавлен delay

--- src/components/messages/CurrentChat/components/NoticeBarter.tsx
+++ src/components/messages/CurrentChat/components/NoticeBarter.tsx
@@ -136,6 +136,7 @@ export const NoticeBarter = memo(function NoticeBarter({ idBarter, userData }: {
                         status: "accepted",
                         threadId: threadId!,
                         created: date,
+                        delay: 60 * 3 * 1000,
                     })
                     refetchOffers()
                 }
