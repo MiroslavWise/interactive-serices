7c47a1e91ecf12aa5d55e1a2e8654d15b19f6417
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2024-01-21 17:42:45 +0300

    исправления в роутах профиля

--- src/components/profile/MyProfilePage/components/M_ContainerAboutProfile.tsx
+++ src/components/profile/MyProfilePage/components/M_ContainerAboutProfile.tsx
@@ -2,17 +2,13 @@
 
 import dayjs from "dayjs"
 import Link from "next/link"
-import Image from "next/image"
-import { useQuery } from "@tanstack/react-query"
+import { useQueries } from "@tanstack/react-query"
 
-import { Button } from "@/components/common"
-import { GeoTagging } from "@/components/common/GeoTagging"
-import { ImageStatic, NextImageMotion } from "@/components/common/Image"
+import { NextImageMotion, Button, GeoTagging } from "@/components/common"
 
 import { useOut } from "@/helpers"
-import { serviceUser } from "@/services/users"
-import { serviceProfile } from "@/services/profile"
-import { useAuth, dispatchNewServicesBanner } from "@/store/hooks"
+import { serviceProfile, serviceUser } from "@/services"
+import { useAuth, dispatchNewServicesBanner } from "@/store"
 
 import styles from "./styles/style.module.scss"
 
@@ -20,16 +16,18 @@ export const M_ContainerAboutProfile = () => {
     const { out } = useOut()
     const userId = useAuth(({ userId }) => userId)
 
-    const { data } = useQuery({
-        queryFn: () => serviceUser.getId(userId!),
-        queryKey: ["user", { userId: userId }],
-        enabled: !!userId,
-    })
-
-    const { data: dataProfile } = useQuery({
-        queryFn: () => serviceProfile.getUserId(userId!),
-        queryKey: ["profile", userId!],
-        enabled: !!userId,
+    const [{data}, {data: dataProfile}] = useQueries({
+        queries: [
+            {
+                queryFn: () => serviceUser.getId(userId!),
+                queryKey: ["user", { userId: userId }],
+                enabled: !!userId,
+            }, {
+                queryFn: () => serviceProfile.getUserId(userId!),
+                queryKey: ["profile", userId!],
+                enabled: !!userId,
+            }
+        ]
     })
 
     const addressMain = data?.res?.addresses?.find((item) => item?.addressType === "main") || null
@@ -39,13 +37,9 @@ export const M_ContainerAboutProfile = () => {
             <div className={styles.blockAboutPhoto}>
                 <div className={styles.blockPhotoAch}>
                     <div className={styles.avatar}>
-                        {dataProfile?.res?.image?.attributes?.url ? (
-                            <NextImageMotion className={styles.photo} src={dataProfile?.res?.image?.attributes?.url} alt="avatar" width={94} height={94} />
-                        ) : (
-                            <ImageStatic src="/png/default_avatar.png" alt="avatar" width={400} height={400} className={styles.photo} />
-                        )}
+                            <NextImageMotion className={styles.photo} src={dataProfile?.res?.image?.attributes?.url!} alt="avatar" width={94} height={94} />
                         {dataProfile?.ok ? (
-                            <Image className={styles.verified} src="/svg/verified-tick.svg" alt="tick" width={24} height={24} unoptimized />
+                            <img className={styles.verified} src="/svg/verified-tick.svg" alt="tick" width={24} height={24} />
                         ) : null}
                     </div>
                 </div>
