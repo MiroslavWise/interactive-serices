0f1c58ae4c38120d5d312d3a0da03e06420f2837
Miroslav Wise <79138594+MiroslavWise@users.noreply.github.com>
2024-01-22 18:27:40 +0300

    update page notifications timer 5*1000

--- src/app/(users)/(profile)/(main)/notifications/page.tsx
+++ src/app/(users)/(profile)/(main)/notifications/page.tsx
@@ -53,7 +53,7 @@ export default function Notifications() {
                             refetch()
                         }
                     })
-                })
+                }, 5 * 1000)
 
                 return () => clearTimeout(timer)
             }
