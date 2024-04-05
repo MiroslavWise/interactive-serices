03f73546bde72dcd7363e5b49ae6bdfbe96967bc
Miroslav Wise <79138594+MiroslavWise@users.noreply.github.com>
2024-04-05 17:04:45 +0300

    исправление номера от 10 цифр

--- src/components/templates/ModalSign/utils/phone-sign-up.schema.ts
+++ src/components/templates/ModalSign/utils/phone-sign-up.schema.ts
@@ -7,8 +7,8 @@ export const schemaPhoneSignUp = z.object({
   phone: z
     .string()
     .trim()
-    .min(11, { message: "Номер телефона состоит из 11 цифр" })
-    .length(11, { message: "Номер телефона состоит из 11 цифр" })
+    .min(10, { message: "Номер телефона состоит из 10-12 цифр" })
+    //.length(11, { message: "Номер телефона состоит из 11 цифр" })
     .default(""),
   checkbox: z.boolean().default(false),
   checkbox_personal_data: z.boolean().default(false),
