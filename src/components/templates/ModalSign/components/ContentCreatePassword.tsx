7ba42a310fc849917d3bec4788cebd21516fdd6b
Miroslav Wise <79138594+MiroslavWise@users.noreply.github.com>
2024-05-13 14:47:34 +0300

    data error input password

--- src/components/templates/ModalSign/components/ContentCreatePassword.tsx
+++ src/components/templates/ModalSign/components/ContentCreatePassword.tsx
@@ -136,7 +136,7 @@ export const ContentCreatePassword = () => {
             <div data-label-input data-password data-test="create-password">
               <label htmlFor={field.name}>Пароль</label>
               <div>
-                <input {...field} placeholder="Введите свой пароль" type={isPass ? "text" : "password"} />
+                <input {...field} placeholder="Введите свой пароль" type={isPass ? "text" : "password"} data-error={!!error} />
                 <img
                   onClick={() => setIsPass((prev) => !prev)}
                   src={isPass ? "/svg/eye.svg" : "/svg/eye-off.svg"}
@@ -160,7 +160,9 @@ export const ContentCreatePassword = () => {
             <div data-label-input data-password data-test="create-password-repeat">
               <label htmlFor={field.name}>Подтвердите пароль</label>
               <div>
-                <input {...field} placeholder="Введите пароль еще раз" type={isPass_ ? "text" : "password"} />
+                <input {...field} placeholder="Введите пароль еще раз" type={isPass_ ? "text" : "password"} 
+data-error={!!error}
+/>
                 <img
                   onClick={() => setIsPass_((prev) => !prev)}
                   src={isPass_ ? "/svg/eye.svg" : "/svg/eye-off.svg"}
