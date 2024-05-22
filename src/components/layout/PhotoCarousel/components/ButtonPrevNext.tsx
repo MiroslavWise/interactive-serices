c6084afff87cb20a14eaee1321d8fb2f42778e74
Miroslav Wise <mipik1997@gmail.com>
2024-05-22 12:55:58 +0300

    issue/1-306

--- src/components/layout/PhotoCarousel/components/ButtonPrevNext.tsx
+++ src/components/layout/PhotoCarousel/components/ButtonPrevNext.tsx
@@ -5,10 +5,10 @@ import { setPrevPhotoCarousel, setNextPhotoCarousel } from "@/store/hooks"
 import styles from "./styles/style.module.scss"
 
 export const ButtonPrevNext = () => {
-    return (
-        <>
-            <div className={styles.prevButton} onClick={setPrevPhotoCarousel}></div>
-            <div className={styles.nextButton} onClick={setNextPhotoCarousel}></div>
-        </>
-    )
+  return (
+    <>
+      <div className={styles.prevButton} onClick={setPrevPhotoCarousel}></div>
+      <div className={styles.nextButton} onClick={setNextPhotoCarousel}></div>
+    </>
+  )
 }
