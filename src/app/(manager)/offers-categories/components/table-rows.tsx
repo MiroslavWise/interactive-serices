1109a02dea408ba5739ff653447fd89f48f46a66
Miroslav Wise <mipik1997@gmail.com>
2024-05-27 18:11:25 +0300

    replace table offers-categories

--- src/app/(manager)/offers-categories/components/table-rows.tsx
+++ src/app/(manager)/offers-categories/components/table-rows.tsx
@@ -1,36 +1,91 @@
 "use client"
 
+import { useMemo, useState } from "react"
 import { useQuery } from "@tanstack/react-query"
 
+import { IQueryOffersCategories, IResponseOffersCategories } from "@/services/offers-categories/types"
+
 import IconEdit from "@/components/icons/IconEdit"
+import IconTrashBlack from "@/components/icons/IconTrashBlack"
 
 import { serviceOffersCategories } from "@/services"
-import { IResponseOffersCategories } from "@/services/offers-categories/types"
+import { Button } from "@/components/common"
 
 interface IPropRow extends IResponseOffersCategories {}
 
-const Row = ({ id, title, slug }: IPropRow) => (
+const Row = ({ id, title, slug, provider }: IPropRow) => (
   <tr>
-    <td></td>
+    <td>
+      {provider} {id}
+    </td>
     <td>{title}</td>
     <td>{slug}</td>
     <td>
       <button type="button">
         <IconEdit />
       </button>
+      <button type="button">
+        <IconTrashBlack />
+      </button>
     </td>
   </tr>
 )
 
+const LoadRow = () => (
+  <tr data-load>
+    <td>
+      <span />
+    </td>
+    <td>
+      <span />
+    </td>
+    <td>
+      <span />
+    </td>
+    <td />
+  </tr>
+)
+
 function TableRows() {
-  const { data } = useQuery({
-    queryFn: () => serviceOffersCategories.get(),
-    queryKey: ["offers-categories"],
+  const [page, setPage] = useState(1)
+
+  const query = useMemo(() => {
+    const q: IQueryOffersCategories = {
+      limit: 15,
+      page: page,
+    }
+
+    return q
+  }, [page])
+
+  const { data, isLoading } = useQuery({
+    queryFn: () => serviceOffersCategories.get({ query: query }),
+    queryKey: ["offers-categories", query],
   })
 
   const list = data?.res || []
+  const total = data?.meta?.total || 0
 
-  return list.map((item) => <Row key={`::ket`} {...item} />)
+  const maxPage = Math.ceil(total / 15) | 1
+  const onIncrement = () => setPage((_) => (_ < maxPage ? _ + 1 : _))
+  const onDecrement = () => setPage((_) => (_ <= 1 ? 1 : _ - 1))
+
+  return (
+    <>
+      <tbody>
+        {isLoading
+          ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((item) => <LoadRow key={`::key::load::item::${item}::`} />)
+          : list.map((item) => <Row key={`::key::item::row::${item.id}::`} {...item} />)}
+      </tbody>
+      <tfoot>
+        <div data-buttons>
+          <Button type="button" typeButton="fill-primary" label="Предыдущая" onClick={onDecrement} disabled={page <= 1} />
+          <Button type="button" typeButton="fill-primary" label="Следующая" onClick={onIncrement} disabled={page >= maxPage} />
+        </div>
+        <span>{total || 0}</span>
+      </tfoot>
+    </>
+  )
 }
 
 TableRows.displayName = "TableRows"
--- src/services/offers-categories/index.ts
+++ src/services/offers-categories/index.ts
@@ -5,7 +5,7 @@ import { wrapperGet, wrapperPost } from "../requestsWrapper"
 const url = "/offers-categories"
 
 export const serviceOffersCategories: IServiceOffersCategories = {
-  get: (query) => wrapperGet({ url, query, cache: "force-cache" }),
+  get: (value) => wrapperGet({ url, query: value?.query, cache: "force-cache" }),
 }
 
 export const postOfferCategory: TPostOfferCategory = ({ body }) => wrapperPost({ url, body })
