d877e6cb9ff0d8b11f948214b5d0b5625314c1b3
Miroslav Wise <79138594+MiroslavWise@users.noreply.github.com>
2024-06-22 11:03:26 +0300

    Проверка роботс файлов

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,24 +1,24 @@
-import { cache } from "react"
+//import { cache } from "react"
 import { MetadataRoute } from "next"
 
-import { getOffers } from "@/services"
+//import { getOffers } from "@/services"
 import env from "@/config/environment"
 
-const getCacheOffers = cache(getOffers)
+//const getCacheOffers = cache(getOffers)
 
-export default async function (): Promise<MetadataRoute.Robots> {
-  const { res } = await getCacheOffers({ order: "DESC", limit: 50 })
+export default function (): MetadataRoute.Robots {
+  //const { res } = await getCacheOffers({ order: "DESC", limit: 50 })
 
-  const items = res || []
-  const customersLinks = Array.from(new Set(items.map((_) => _.userId)))
-    .map((_) => [`/customer/${_}`, `/customer/${_}?provider=discussion`, `/customer/${_}?provider=alert`])
-    .flat()
+  //const items = res || []
+  //const customersLinks = Array.from(new Set(items.map((_) => _.userId)))
+    //.map((_) => [`/customer/${_}`, `/customer/${_}?provider=discussion`, `/customer/${_}?provider=alert`])
+    //.flat()
 
   return {
     rules: [
       {
         userAgent: "*",
-        allow: ["/", "/legal/ads-agreement/", "/legal/privacy-policy/", ...customersLinks, "/legal/terms/", "/registration"],
+        allow: ["/", "/legal/ads-agreement/", "/legal/privacy-policy/", "/legal/terms/", "/registration"],
       },
     ],
     sitemap: `${env.server.host}/sitemap.xml`,
