430767779b02a98c539e1bf33f7201122bf51a02
Miroslav Wise <mipik1997@gmail.com>
2024-06-22 14:44:03 +0300

    f

--- src/components/common/Card/CardBallon/components/ItemProfile.tsx
+++ src/components/common/Card/CardBallon/components/ItemProfile.tsx
@@ -10,6 +10,7 @@ import { NextImageMotion } from "@/components/common/Image"
 
 import { getTestimonials } from "@/services"
 import { dispatchProfilePublic } from "@/store"
+import Link from "next/link"
 
 function ItemProfile({ user }: { user: IUserOffer }) {
   const { id, firstName, lastName, image } = user ?? {}
@@ -43,18 +44,19 @@ function ItemProfile({ user }: { user: IUserOffer }) {
   return (
     <section className="mt-1 w-full pt-0.625 border-t-[1px] border-t-grey-stroke-light border-solid">
       <div className="flex flex-row items-center justify-between w-full gap-0.625">
-        <div
+        <Link
           className="flex flex-row items-center gap-0.625"
           onClick={(event) => {
             event.stopPropagation()
-            handleProfile()
+            // handleProfile()
           }}
+          href={{ pathname: `/customer/${id}` }}
         >
           <div className="relative h-6 w-6 rounded-md bg-BG-second overflow-hidden [&>img]:absolute [&>img]:inset-0 [&>img]:h-full [&>img]:w-full">
             <NextImageMotion src={image?.attributes?.url!} alt="avatar" width={24} height={24} />
           </div>
           <p className="text-text-primary text-sm font-normal line-clamp-1 whitespace-nowrap">{name}</p>
-        </div>
+        </Link>
         {rating ? (
           <div className="flex flex-row items-center">
             <div className="w-4 h-4 p-[0.1875rem] flex items-center justify-center [&>svg]:w-0.625 [&>svg]:h-0.625">
