888b51d6572baee85162a35b40dfb2240b4a584b
sheira <ole.larssen777@gmail.com>
2024-06-22 17:37:48 +0530

    domains: [process.env.NEXT_PUBLIC_DOMAIN || ""],

--- next.config.js
+++ next.config.js
@@ -15,6 +15,6 @@ module.exports = {
     loader: "custom",
     loaderFile: "./src/helpers/functions/myImageLoader.ts",
     formats: ["image/avif", "image/webp"],
-    domains: [process.env.NEXT_PUBLIC_DOMAIN],
+    domains: [process.env.NEXT_PUBLIC_DOMAIN || ""],
   },
 }
