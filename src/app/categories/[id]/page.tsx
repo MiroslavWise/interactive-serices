98b54a5559f9bcca44f522e89781cd37d188c775
Miroslav Wise <mipik1997@gmail.com>
2024-06-26 19:47:30 +0300

    проверка на запросы

--- src/app/categories/[id]/page.tsx
+++ src/app/categories/[id]/page.tsx
@@ -1,1 +1,16 @@
+import { cache } from "react"
+
+import { getOffersCategories } from "@/services"
+
+const get = cache(getOffersCategories)
+
+export async function generateStaticParams() {
+  const { res } = await get()
+  const items = res || []
+
+  return items.map((_) => ({
+    id: String(_.id),
+  }))
+}
+
 export default () => <></>
