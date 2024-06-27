69153f950d733118c2f620584d0d6ffce6ada148
Miroslav Wise <mipik1997@gmail.com>
2024-06-27 23:05:52 +0300

    fix issue/1-469 issue/1-470

--- src/components/common/Card/CardBallon/components/GeoData.tsx
+++ src/components/common/Card/CardBallon/components/GeoData.tsx
@@ -1,6 +1,10 @@
 import { useMemo } from "react"
 
 import { IResponseOffers } from "@/services/offers/types"
+import IconMapWhite from "@/components/icons/IconMapWhite"
+import Link from "next/link"
+import { dispatchMapCoordinates } from "@/store"
+import IconArrowRight from "@/components/icons/IconArrowRight"
 
 function GeoData({ offer }: { offer: IResponseOffers }) {
   const geo = useMemo(() => {
@@ -8,31 +12,33 @@ function GeoData({ offer }: { offer: IResponseOffers }) {
 
     const address = offer.addresses[0]
 
-    const city = address?.city
-    const street = address?.street
-    const house = address?.house
+    const additional = address?.additional?.replace(`${address?.country}, `, "").replace(`${address?.region}, `, "") ?? ""
 
-    const filter = [city, street, house].filter(Boolean).join(", ")
-
-    if (filter.length === 0) {
-      return address.additional
-    }
-
-    return filter
+    return additional
   }, [offer?.addresses])
 
+  const geoData = offer?.addresses?.length > 0 ? offer?.addresses[0] : null
+
   return geo ? (
-    <footer className="w-full flex flex-row items-start gap-1">
-      <div className=" relative w-4 h-4 p-2">
-        <img
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-4 h-4"
-          src="/svg/geo-marker.svg"
-          alt="geo"
-          width={16}
-          height={16}
-        />
+    <footer className="w-full grid grid-cols-[1.5rem_minmax(0,1fr)_1.25rem] items-start gap-2">
+      <div className="relative w-6 h-6 p-3 rounded-xl bg-element-accent-1 [&>svg]:absolute [&>svg]:top-1/2 [&>svg]:left-1/2 [&>svg]:-translate-x-1/2 [&>svg]:-translate-y-1/2 [&>svg]:w-[0.9rem] [&>svg]:h-[0.9rem]">
+        <IconMapWhite />
       </div>
-      <span className=" text-text-secondary text-[0.8125rem] leading-4 font-normal">{geo}</span>
+      <span className="text-text-secondary text-sm font-normal pt-0.125">{geo}</span>
+      <Link
+        href={{ pathname: "/" }}
+        onClick={() => {
+          if (geoData) {
+            dispatchMapCoordinates({
+              zoom: 17,
+              coordinates: geoData?.coordinates?.split(" ")?.map(Number),
+            })
+          }
+        }}
+        className="w-5 h-5 p-0.625 cursor-pointer relative [&>svg]:absolute [&>svg]:top-1/2 [&>svg]:left-1/2 [&>svg]:-translate-x-1/2 [&>svg]:-translate-y-1/2 [&>svg]:w-5 [&>svg]:h-5"
+      >
+        <IconArrowRight />
+      </Link>
     </footer>
   ) : null
 }
