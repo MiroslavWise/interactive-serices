4492090a9fdd5ad480679c900434c1347d738fa0
Miroslav Wise <mipik1997@gmail.com>
2024-07-10 20:41:26 +0300

    asdf

--- src/app/chat/@list/components/HeaderAndNavigation.tsx
+++ src/app/chat/@list/components/HeaderAndNavigation.tsx
@@ -3,7 +3,7 @@ import NavigationSelectChat from "./NavigationSelectChat"
 
 function HeaderAndNavigation() {
   return (
-    <header className="w-full pt-5 px-5 flex flex-col gap-5">
+    <header className="w-full pt-1 md:pt-5 px-5 flex flex-col gap-4 md:gap-5">
       <SearchInput />
       <NavigationSelectChat />
     </header>
--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -13,7 +13,7 @@ interface IProps extends IPropsChat {
 
 export default ({ children, list }: IProps) => {
   return (
-    <main className="w-full h-screen md:px-6 md:pb-6 md:pt-[calc(var(--height-header-nav-bar)_+_1.5rem)] md:grid md:grid-cols-[21.25rem_minmax(0,1fr)] md:gap-6">
+    <main className="w-full relative h-screen md:px-6 md:pb-6 md:pt-[calc(var(--height-header-nav-bar)_+_1.5rem)] md:grid md:grid-cols-[21.25rem_minmax(0,1fr)] md:gap-6">
       {list}
       {children}
     </main>
