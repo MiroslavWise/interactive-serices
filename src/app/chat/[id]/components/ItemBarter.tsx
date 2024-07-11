ee2ad6bf1cd6d261f2d1b9751fe5d2b22408d8b0
Miroslav Wise <mipik1997@gmail.com>
2024-07-11 16:40:14 +0300

    проверка

--- src/app/chat/[id]/components/ItemBarter.tsx
+++ src/app/chat/[id]/components/ItemBarter.tsx
@@ -68,7 +68,7 @@ function ItemBarter({ thread }: { thread: IResponseThread }) {
         : null
 
     return (
-      <div className="w-full flex flex-col items-center gap-3 mt-auto">
+      <div className="w-full flex flex-col items-center gap-3 mt-auto max-md:sticky max-md:top-0">
         <article className="w-fit min-w-16 px-3 h-[1.6875rem] rounded-[0.84375rem] py-1 flex items-center justify-center bg-BG-time">
           <time className="text-text-button text-[0.8125rem] font-normal">{formatOfMMMM(dataB?.created || new Date(), "dd MMMM")}</time>
         </article>
