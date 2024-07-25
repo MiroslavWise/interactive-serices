b21a0ffdbf9d3b155e89970c171b27a7fde5c38e
Miroslav Wise <mipik1997@gmail.com>
2024-07-25 14:25:20 +0300

    fix

--- src/components/common/Card/CardBallon/components/HeaderTimeDots.tsx
+++ src/components/common/Card/CardBallon/components/HeaderTimeDots.tsx
@@ -3,12 +3,20 @@
 import { memo } from "react"
 
 import { IResponseOffers } from "@/services/offers/types"
-import { ITEMS_LINK } from "@/components/common/maps/CopyLinks"
+
+import IconShare from "@/components/icons/IconShare"
+import IconComplaint from "@/components/icons/IconComplaint"
 import { IconDotsHorizontal } from "@/components/icons/IconDotsHorizontal"
 
-import { useToast } from "@/helpers/hooks/useToast"
-import { daysAgo, useOutsideClickEvent } from "@/helpers"
 import { cx } from "@/lib/cx"
+import env from "@/config/environment"
+import { encryptedOffer } from "@/helpers/cript"
+import { useToast } from "@/helpers/hooks/useToast"
+import { dispatchComplaintModalOffer } from "@/store"
+import { daysAgo, useOutsideClickEvent } from "@/helpers"
+
+const TITLE_SHARE = "Поделиться"
+const TITLE_COMPLAINT = "Пожаловаться"
 
 function HeaderTimeDots({ offer }: { offer: IResponseOffers }) {
   const [visible, setVisible, ref] = useOutsideClickEvent()
@@ -35,27 +43,56 @@ function HeaderTimeDots({ offer }: { offer: IResponseOffers }) {
         <article
           className={cx(
             "absolute top-[calc(100%_+_0.25rem)] right-0 rounded-xl p-3 bg-BG-second w-[13.5rem] flex flex-col gap-0.125 opacity-0 invisible z-[120]",
+            "[&>*]:grid [&>*]:grid-cols-[1.25rem_minmax(0,1fr)] [&>*]:gap-0.625 [&>*]:items-center [&>*]:py-2 [&>*]:px-0.375 [&>*]:rounded-md",
+            "[&>*>span]:text-text-primary [&>*>span]:text-sm [&>*>span]:font-normal [&>*>span]:text-left [&>*>span]:whitespace-nowrap",
+            "[&>*>div]:w-5 [&>*>div]:h-5  [&>*>div]:relative  [&>*>div]:p-0.625",
+            "hover:[&>*]:bg-grey-field",
+            "[&>*>div>svg]:w-5 [&>*>div>svg]:h-5 [&>*>div>svg]:absolute [&>*>div>svg]:top-1/2 [&>*>div>svg]:left-1/2 [&>*>div>svg]:-translate-x-1/2 [&>*>div>svg]:-translate-y-1/2",
             visible && "!opacity-100 !visible",
           )}
         >
-          {ITEMS_LINK({ offer, onSimpleMessage }).map((item) => (
-            <a
-              className="w-full py-2 px-0.375 flex items-center gap-0.625 rounded-[0.375rem] bg-transparent hover:bg-grey-field focus:bg-grey-field"
-              key={`::key::copy::${item.label}::`}
-              onClick={(event) => {
-                event.stopPropagation()
-                if (item.linkCopy) {
-                  item.linkCopy()
-                }
-                setVisible(false)
-              }}
-            >
-              <div className="relative w-5 h-5 p-0.625 [&>svg]:absolute [&>svg]:w-5 [&>svg]:h-5 [&>svg]:top-1/2 [&>svg]:left-1/2 [&>svg]:-translate-x-1/2 [&>svg]:-translate-y-1/2">
-                {item.icon}
-              </div>
-              <span className="text-text-primary text-sm font-normal whitespace-nowrap">{item.label}</span>
-            </a>
-          ))}
+          <a
+            title={TITLE_SHARE}
+            aria-label={TITLE_SHARE}
+            aria-labelledby={TITLE_SHARE}
+            onClick={(event) => {
+              const hash = encryptedOffer(offer.id)
+              const url = `${env.server.host}/offer/${hash}`
+              if (!!window.navigator.share!) {
+                navigator.share({
+                  title: offer.title!,
+                  text: offer?.addresses[0] ? offer.addresses[0]?.additional! : "",
+                  url: url,
+                })
+              } else {
+                navigator.clipboard.writeText(url)
+                onSimpleMessage("Ссылка скопирована")
+              }
+              event.stopPropagation()
+            }}
+          >
+            <div>
+              <IconShare />
+            </div>
+            <span>{TITLE_SHARE}</span>
+          </a>
+          <a
+            title={TITLE_COMPLAINT}
+            aria-label={TITLE_COMPLAINT}
+            aria-labelledby={TITLE_COMPLAINT}
+            onClick={(event) => {
+              console.log("onClick TITLE_COMPLAINT: ")
+              event.stopPropagation()
+              event.preventDefault()
+              dispatchComplaintModalOffer({ offer })
+              setVisible(false)
+            }}
+          >
+            <div>
+              <IconComplaint />
+            </div>
+            <span className="!text-text-error">{TITLE_COMPLAINT}</span>
+          </a>
         </article>
       </div>
     </div>
