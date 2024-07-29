a0547ab890fe389560e0c80ca080d2cda9e4b09f
Miroslav Wise <mipik1997@gmail.com>
2024-07-29 11:16:42 +0300

    fix

--- src/app/(profile)/(main)/notifications/layout.tsx
+++ src/app/(profile)/(main)/notifications/layout.tsx
@@ -9,9 +9,17 @@ export const metadata: Metadata = {
   twitter: { title: "Уведомления" },
 }
 
-import styles from "./layout.module.scss"
 import main from "../layout.module.scss"
 
 export default function LayoutNotification({ children }: { children: ReactNode }) {
-  return <section className={cx(main.wrapperInsideContainer, styles.wrapper)}>{children}</section>
+  return (
+    <section
+      className={cx(
+        main.wrapperInsideContainer,
+        "md:h-[calc(100vh_-_var(--height-header-nav-bar))] hidden md:flex flex-col gap-4 pt-6 !-mt-6 overflow-x-hidden overflow-y-auto",
+      )}
+    >
+      {children}
+    </section>
+  )
 }
