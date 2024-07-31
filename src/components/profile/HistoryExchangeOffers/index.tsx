4006897635734e207bf1875ee14abe6640c7f440
Miroslav Wise <mipik1997@gmail.com>
2024-07-31 10:48:25 +0300

    fix styles

--- src/components/profile/HistoryExchangeOffers/index.tsx
+++ src/components/profile/HistoryExchangeOffers/index.tsx
@@ -3,21 +3,18 @@
 import { useState } from "react"
 
 import { EnumStatusBarter } from "@/types/enum"
-import type { THistoryExchangeOffers } from "./types/types"
-import type { ISegmentValues } from "@/components/common/Segments/types"
+import { type ISegmentValues } from "@/components/common/Segments/types"
 
 import { Header } from "./components/Header"
 import { SentenceCards } from "./components/SentenceCards"
 
 import { SEGMENTS } from "./constants"
 
-import styles from "./styles/style.module.scss"
-
-export const HistoryExchangeOffers: THistoryExchangeOffers = ({}) => {
+export const HistoryExchangeOffers = ({}) => {
   const [value, setValue] = useState<ISegmentValues<EnumStatusBarter>>(SEGMENTS[0])
 
   return (
-    <aside className={styles.container}>
+    <aside className="fixed w-full max-w-[24.375rem] hidden md:flex flex-col gap-6 bg-BG-second z-[2] overflow-hidden rounded-[2rem] top-[calc(var(--height-header-nav-bar)_+_1.5rem)] right-6 bottom-6 h-[calc(100vh_-_var(--height-header-nav-bar)_-_3rem)] pt-5">
       <Header {...{ value, setValue }} />
       <SentenceCards {...{ value }} />
     </aside>
