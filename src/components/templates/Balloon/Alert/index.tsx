bbe605a23b0af13984a80381a798250dafc1b501
Miroslav Wise <mipik1997@gmail.com>
2024-08-09 19:41:30 +0300

    fix issue/1-612

--- src/components/templates/Balloon/Alert/index.tsx
+++ src/components/templates/Balloon/Alert/index.tsx
@@ -25,7 +25,7 @@ function BalloonAlert() {
         className="w-full rounded-t-3xl md:rounded-t-[2rem] grid grid-cols-[1.5rem_minmax(0,1fr)] items-start gap-2.5 p-5 border-b border-solid border-grey-stroke-light overflow-hidden"
         data-color={EnumTypeProvider.alert}
       >
-        <div data-img>
+        <div className="w-6 h-6 relative p-3 *:absolute *:top-1/2 *:left-1/2 *:-translate-x-1/2 *:-translate-y-1/2 *:w-6 *h-6">
           <IconAlertBalloon />
         </div>
         <h3 style={{ color: "var(--text-primary)" }}>{title ? title : "SOS-cообщение"}</h3>
--- src/components/templates/Balloon/Offer/index.tsx
+++ src/components/templates/Balloon/Offer/index.tsx
@@ -21,7 +21,9 @@ function BalloonOffer() {
         className="w-full rounded-t-3xl md:rounded-t-[2rem] grid grid-cols-[1.5rem_minmax(0,1fr)] items-start gap-2.5 p-5 border-b border-solid border-grey-stroke-light overflow-hidden"
         data-color={EnumTypeProvider.offer}
       >
-        <div data-category-img>{offer?.categoryId ? <ImageCategory id={offer?.categoryId!} /> : null}</div>
+        <div className="w-6 h-6 relative p-3 *:absolute *:top-1/2 *:left-1/2 *:-translate-x-1/2 *:-translate-y-1/2 *:w-6 *h-6">
+          {offer?.categoryId ? <ImageCategory id={offer?.categoryId!} /> : null}
+        </div>
         <h3>{offer?.category?.title}</h3>
       </header>
       <div data-container className="w-full p-0 md:rounded-b-[2rem]">
