f8df0cc03df838a5416e3a030379c89fd94e5f90
Miroslav Wise <mipik1997@gmail.com>
2024-08-15 11:52:58 +0300

    fix styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,11 +5,11 @@ export const dynamicParams = false
 
 export default function NotFound() {
   return (
-    <div className="not-found-page">
-      <h2>Упс! Похоже, вы заблудились…</h2>
-      <p>
+    <div className="w-full h-full gap-12 flex flex-col items-center justify-center px-5 py-5">
+      <h2 className="text-text-accent text-center font-semibold text-4xl">Упс! Похоже, вы заблудились…</h2>
+      <p className="text-text-primary font-medium text-base">
         Извините, но страница, которую вы ищете, не существует. Пожалуйста, проверьте введенный вами URL или вернитесь на&nbsp;
-        <Link href="/" prefetch>
+        <Link href="/" prefetch className="text-text-accent cursor-pointer">
           главную
         </Link>
         &nbsp;страницу.
