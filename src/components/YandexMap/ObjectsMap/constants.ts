2d716b9179ed3ae7cf1cc8be36cdd17e36cdcc01
Miroslav Wise <mipik1997@gmail.com>
2024-08-20 19:54:14 +0300

    откат пинов

--- src/components/YandexMap/ObjectsMap/constants.ts
+++ src/components/YandexMap/ObjectsMap/constants.ts
@@ -2,9 +2,9 @@ import { EnumTypeProvider } from "@/types/enum"
 
 export const TYPE_ICON: Record<EnumTypeProvider, string> = {
   [EnumTypeProvider.profile]: "",
-  [EnumTypeProvider.offer]: "/map/droplet/offer.png",
-  [EnumTypeProvider.alert]: "/map/droplet/sos.png",
-  [EnumTypeProvider.discussion]: "/map/droplet/discussion.png",
+  [EnumTypeProvider.offer]: "/map/droplet/offer.svg",
+  [EnumTypeProvider.alert]: "/map/droplet/sos.svg",
+  [EnumTypeProvider.discussion]: "/map/droplet/discussion.svg",
   [EnumTypeProvider.barter]: "",
   [EnumTypeProvider.threads]: "",
 }
