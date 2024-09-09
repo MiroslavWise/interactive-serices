4e8f21b6c64ea70257733792409c712b59074a71
Miroslav Wise <mipik1997@gmail.com>
2024-09-09 14:40:37 +0300

    fix

--- src/app/post/[id]/layout.tsx
+++ src/app/post/[id]/layout.tsx
@@ -19,6 +19,8 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
   if (!!data) {
     obj.title = data.title
 
+    obj.keywords = data.title.split(" ").map((_) => _.replace(/\w/g, ""))
+
     const note = data?.notes?.find((item) => item?.main)
 
     if (note) {
