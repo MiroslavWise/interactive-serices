9665e234f253a6ff2db36dae885ec8c5d2eb8fb2
Miroslav Wise <mipik1997@gmail.com>
2024-11-18 15:37:31 +0300

    f

--- src/app/(layout)/customer/[userId]/@offers/components/ItemServiceImages-client.tsx
+++ src/app/(layout)/customer/[userId]/@offers/components/ItemServiceImages-client.tsx
@@ -1,16 +1,29 @@
 "use client"
 
-import { useRef } from "react"
+import { useMemo, useRef } from "react"
 
 import { type IImageData } from "@/types/type"
 
 import { NextImageMotion } from "@/components/common"
 
 import { cx } from "@/lib/cx"
-import { dispatchPhotoCarousel } from "@/store"
+import { dispatchPhotoCarousel, dispatchVideoStream } from "@/store"
+import IconPlayCircle from "@/components/icons/IconPlayCircle"
 
 function ItemServiceImagesClient({ images }: { images: IImageData[] }) {
   const refImages = useRef<HTMLDivElement>(null)
+
+  const items = useMemo(() => {
+    const array = []
+
+    for (const item of images) {
+      if (item.attributes.mime.includes("image") || item.attributes.mime.includes("video")) {
+        array.push(item)
+      }
+    }
+
+    return array
+  }, [images])
 
   return (
     <section
@@ -28,30 +41,64 @@ function ItemServiceImagesClient({ images }: { images: IImageData[] }) {
         }
       }}
     >
-      {images.map((item) => (
-        <NextImageMotion
-          key={`::key::image::offer::${item.id}::`}
-          src={item.attributes.url}
-          className="h-[4.875rem] rounded-lg w-[4.375rem] aspect-[4.375/4.875] cursor-pointer"
-          alt="offer-image"
-          width={140}
-          height={156}
-          onClick={(event) => {
-            event.stopPropagation()
-            const photos = images.map((item) => ({
-              url: item?.attributes?.url!,
-              id: item?.id,
-              hash: item?.attributes?.blur,
-            }))
-            dispatchPhotoCarousel({
-              visible: true,
-              photos: photos,
-              idPhoto: item?.id!,
-            })
-          }}
-          hash={item.attributes?.blur}
-        />
-      ))}
+      {items.map((item) => {
+        if (item.attributes.mime.includes("image"))
+          return (
+            <NextImageMotion
+              key={`::key::image::offer::${item.id}::`}
+              src={item.attributes.url}
+              className="h-[4.875rem] rounded-lg w-[4.375rem] aspect-[4.375/4.875] cursor-pointer"
+              alt="offer-image"
+              width={140}
+              height={156}
+              onClick={(event) => {
+                event.stopPropagation()
+                const photos = []
+                for (const item of items) {
+                  if (item.attributes.mime.includes("image")) {
+                    photos.push({
+                      url: item?.attributes?.url!,
+                      id: item?.id,
+                      hash: item?.attributes?.blur,
+                    })
+                  }
+                }
+                dispatchPhotoCarousel({
+                  visible: true,
+                  photos: photos,
+                  idPhoto: item?.id!,
+                })
+              }}
+              hash={item.attributes?.blur}
+            />
+          )
+
+        if (item.attributes.mime.includes("video"))
+          return (
+            <div
+              key={`:d:s:a:${item.id}:p:p:`}
+              className="relative rounded-lg overflow-hidden h-[4.875rem] w-[4.375rem] aspect-[4.375/4.875]"
+              data-video
+            >
+              <video className="w-full h-full cursor-pointer object-cover absolute inset-0">
+                <source src={item.attributes.url.replace("?format=webp", "")} type={item.attributes.mime} />
+                <source src={item.attributes.url.replace("?format=webp", "")} type="video/webm" />
+              </video>
+              <button
+                type="button"
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-12 h-12 *:w-8 *:h-8"
+                onClick={(event) => {
+                  event.stopPropagation()
+                  dispatchVideoStream(item.attributes.url, item.attributes.mime)
+                }}
+              >
+                <IconPlayCircle />
+              </button>
+            </div>
+          )
+
+        return null
+      })}
     </section>
   )
 }
