46361c88b3f874e8443ca3fe5dda9b30c85a4b1d
Miroslav Wise <mipik1997@gmail.com>
2024-12-03 09:47:21 +0300

    issue/1-866

--- src/components/content/BannerAbout/index.tsx
+++ src/components/content/BannerAbout/index.tsx
@@ -1,12 +1,5 @@
-"use client"
-
-import { BannerAboutMobile } from "./components/Mobile"
 import { BannerAboutDesktop } from "./components/Desktop"
 
-import { useResize } from "@/helpers"
-
 export const BannerAbout = () => {
-  const { isTablet } = useResize()
-
-  return isTablet ? <BannerAboutMobile /> : <BannerAboutDesktop />
+  return <BannerAboutDesktop /> 
 }
