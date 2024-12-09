fa3781a0d98bc6f19990a52c2eae658af6bf1417
Miroslav Wise <mipik1997@gmail.com>
2024-12-09 15:21:44 +0300

    ussue/1-876

--- src/components/templates/Cookies/index.tsx
+++ src/components/templates/Cookies/index.tsx
@@ -13,8 +13,8 @@ export default function CookiesToast() {
   return (
     <article
       className={cx(
-        "fixed left-5 bottom-5 -z-10 opacity-0 invisible p-4 bg-element-accent-2 rounded-2xl shadow-menu-absolute w-full md:max-w-[22.5rem] flex flex-wrap items-center gap-4 max-md:right-5 max-md:max-w-[calc(100%_-_2.5rem)] max-md:bottom-[calc(1.25rem_+_var(--height-mobile-footer-nav))]",
-        visible && "!z-[1000] !opacity-100 !visible",
+        "fixed left-5 bottom-5 p-2 bg-element-accent-2 rounded-xl shadow-menu-absolute w-fit flex flex-wrap items-center gap-4 max-md:right-5 max-md:max-w-[calc(100%_-_2.5rem)] max-md:bottom-[calc(1.25rem_+_var(--height-mobile-footer-nav))]",
+        visible ? "z-[1000] opacity-100 visible" : "-z-10 opacity-0 invisible",
       )}
     >
       <p className="text-text-tab text-justify text-sm font-light [&>a]:font-medium [&>a]:text-link-color *:no-underline">
