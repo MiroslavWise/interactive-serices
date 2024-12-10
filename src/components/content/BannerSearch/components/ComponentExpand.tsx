e7f4611fd730e3809c2ca222f9413438bbd3a534
Miroslav Wise <mipik1997@gmail.com>
2024-12-10 13:23:00 +0300

    issue/1-888

--- src/components/content/BannerSearch/components/ComponentExpand.tsx
+++ src/components/content/BannerSearch/components/ComponentExpand.tsx
@@ -11,11 +11,11 @@ interface IProps {
   title: string
 }
 
-function ComponentExpand({ is, on, length }: IProps) {
+function ComponentExpand({ is, on, length, title }: IProps) {
   return (
     <a className={cx(length > 0 ? "flex flex-row" : "hidden", "w-full items-center justify-between")}>
       <div className="flex items-center gap-2">
-        <span className="text-sm font-medium text-text-primary">Сервисы</span>
+        <span className="text-sm font-medium text-text-primary">{title}</span>
         <div className="relative flex items-center justify-center h-[1.1875rem] min-w-[1.1875rem] w-fit rounded-[0.59375rem] bg-element-accent-1">
           <span className="text-[0.625rem] text-center text-text-button font-semibold">{length}</span>
         </div>
