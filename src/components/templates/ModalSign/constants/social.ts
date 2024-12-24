e80423af77a3f3579aaf225d779194bc76c93931
Miroslav Wise <mipik1997@gmail.com>
2024-12-24 15:58:18 +0300

    asdf

--- src/components/templates/ModalSign/constants/social.ts
+++ src/components/templates/ModalSign/constants/social.ts
@@ -1,39 +1,39 @@
 import type { ILinkSocial } from "../types/types"
 
 export const ITEMS_SOCIAL_LINK: ILinkSocial[] = [
-    // {
-    //     value: "google",
-    //     srcWorking: "/icons/fill/google.svg",
-    //     srcNotWorking: "/icons/fill/disabled/google.svg",
-    //     path: "/google/login",
-    //     isWorkingLink: true,
-    // },
-    {
-        value: "telegram",
-        srcWorking: "/icons/fill/telegram.svg",
-        srcNotWorking: "/icons/fill/disabled/telegram.svg",
-        path: "/telegram/login",
-        isWorkingLink: true,
-    },
-    // {
-    //     value: "apple",
-    //     srcWorking: "/icons/fill/apple.svg",
-    //     srcNotWorking: "/icons/fill/disabled/apple.svg",
-    //     path: "/apple/login",
-    //     isWorkingLink: false,
-    // },
-  //  {
-     //   value: "vk",
-    //    srcWorking: "/icons/fill/vk.svg",
-   //     srcNotWorking: "/icons/fill/vk.svg",
-   //     path: "/vk/login",
-   //     isWorkingLink: true,
-   // },
-    {
-        value: "yandex",
-        srcWorking: "/icons/fill/yandex.svg",
-        srcNotWorking: "/icons/fill/disabled/apple.svg",
-        path: "/yandex/login",
-        isWorkingLink: true,
-    },
+  // {
+  //     value: "google",
+  //     srcWorking: "/icons/fill/google.svg",
+  //     srcNotWorking: "/icons/fill/disabled/google.svg",
+  //     path: "/google/login",
+  //     isWorkingLink: true,
+  // },
+  {
+    value: "telegram",
+    srcWorking: "/icons/fill/telegram.svg",
+    srcNotWorking: "/icons/fill/disabled/telegram.svg",
+    path: "/telegram/login",
+    isWorkingLink: true,
+  },
+  // {
+  //     value: "apple",
+  //     srcWorking: "/icons/fill/apple.svg",
+  //     srcNotWorking: "/icons/fill/disabled/apple.svg",
+  //     path: "/apple/login",
+  //     isWorkingLink: false,
+  // },
+  {
+    value: "vk",
+    srcWorking: "/icons/fill/vk.svg",
+    srcNotWorking: "/icons/fill/vk.svg",
+    path: "/vk/login",
+    isWorkingLink: true,
+  },
+  {
+    value: "yandex",
+    srcWorking: "/icons/fill/yandex.svg",
+    srcNotWorking: "/icons/fill/disabled/apple.svg",
+    path: "/yandex/login",
+    isWorkingLink: true,
+  },
 ]
