95f667ffefa3a7ed259d2ddd02b30a136e0c6131
Miroslav Wise <mipik1997@gmail.com>
2025-01-09 15:41:14 +0300

    cld

--- src/components/common/Card/CardPost/index.tsx
+++ src/components/common/Card/CardPost/index.tsx
@@ -1,4 +1,5 @@
 import { EnumTypeProvider } from "@/types/enum"
+import { INotes } from "@/services/notes/types"
 import { type IPosts } from "@/services/posts/types"
 
 import GeoData from "./components/GeoData"
@@ -7,12 +8,11 @@ import IconNote from "@/components/icons/IconNote"
 import ComponentDots from "./components/ComponentDots"
 import CommentsCount from "./components/CommentsCount"
 import ItemProfile from "../CardBallon/components/ItemProfile"
+import ItemImages from "@/components/templates/Balloon/Offer/components/ItemImages"
 
 import { cx } from "@/lib/cx"
 import { nameTitle } from "@/lib/names"
 import { dispatchBallonPost } from "@/store"
-import ItemImages from "@/components/templates/Balloon/Offer/components/ItemImages"
-import { useMemo } from "react"
 
 interface IProps {
   post: IPosts
@@ -20,6 +20,19 @@ interface IProps {
   className?: string
 }
 
+function replaceImageFiles(notes: INotes[]) {
+  const array = []
+
+  const noteMain = notes.find((item) => item.main)?.images ?? []
+
+  for (const item of noteMain) {
+    if (item.attributes.mime.includes("image") || item.attributes.mime.includes("video")) {
+      array.push(item)
+    }
+  }
+
+  return array
+}
 function CardPost({ post, className, ref }: IProps) {
   const { title, user, notes = [], id } = post ?? {}
 
@@ -28,23 +41,16 @@ function CardPost({ post, className, ref }: IProps) {
   }
 
   const description = notes.find((item) => item.main)?.description ?? title
-
-  const replaceImageFiles = useMemo(() => {
-    const array = []
-
-    const noteMain = notes.find((item) => item.main)?.images ?? []
-
-    for (const item of noteMain) {
-      if (item.attributes.mime.includes("image") || item.attributes.mime.includes("video")) {
-        array.push(item)
-      }
-    }
-
-    return array
-  }, [notes])
+  const replaceImage = replaceImageFiles(notes)
 
   return (
-    <article className={cx("w-full rounded-2xl cursor-pointer flex flex-col bg-card-yellow", className)} onClick={handle} ref={ref}>
+    <article
+      className={cx(
+        "w-full border border-solid rounded-2xl cursor-pointer flex flex-col bg-card-yellow gap-3 p-4 border-card-border-yellow",
+        className,
+      )}
+      onClick={handle}
+    >
       {/* <header
         className={cx(
           "[background:var(--more-red-gradient)] w-full py-1.5 px-2.5 flex-row gap-2 items-center justify-center rounded-t-2xl",
@@ -56,37 +62,28 @@ function CardPost({ post, className, ref }: IProps) {
         </div>
         <span className="text-text-button text-xs font-medium">Щедрое сердце</span>
       </header> */}
-      <section
-        className={cx(
-          "flex flex-col gap-3 p-4 w-full border-solid border-card-border-yellow",
-          // !!urgent ?
-          // "border-l border-r border-b rounded-b-2xl" :
-          "border rounded-2xl",
-        )}
-      >
-        <ComponentDots post={post} />
-        <header className="w-full grid grid-cols-[1.625rem_minmax(0,1fr)] gap-3 items-start">
-          <div className="relative w-[1.625rem] h-[1.625rem] p-[0.8125rem] *:absolute *:-translate-x-1/2 *:-translate-y-1/2 *:left-1/2 *:top-1/2 *:w-[1.625rem] *:h-[1.625rem]">
-            <IconPost />
-          </div>
-          <h3 className="text-base font-semibold text-text-primary text-ellipsis line-clamp-2">{title ?? "Заголовок поста"}</h3>
-        </header>
-        <p className="whitespace-pre-wrap text-text-primary text-sm font-normal line-clamp-4 text-ellipsis">{description}</p>
-        {replaceImageFiles.length > 0 ? <ItemImages images={replaceImageFiles} /> : null}
-        <div className="w-full flex flex-row items-center justify-start gap-2.5 *:h-[1.875rem] *:rounded-[0.9375rem]">
-          <div className="px-2.5 w-fit bg-[var(--card-bg-yellow)] py-[0.3125rem] gap-1 grid grid-cols-[1.25rem_minmax(0,1fr)] items-center">
-            <div className="w-5 h-5 relative p-2.5 *:absolute *:top-1/2 *:left-1/2 *:-translate-x-1/2 *:-translate-y-1/2 *:w-5 *:h-5 *:scale-90">
-              <IconNote />
-            </div>
-            <span className="text-text-primary text-xs font-medium whitespace-nowrap">
-              {notes?.length || 0} {nameTitle(notes?.length, EnumTypeProvider.NOTE)}
-            </span>
-          </div>
-          <CommentsCount id={id!} />
+      <ComponentDots post={post} />
+      <header className="w-full grid grid-cols-[1.625rem_minmax(0,1fr)] gap-3 items-start">
+        <div className="relative w-[1.625rem] h-[1.625rem] p-[0.8125rem] *:absolute *:-translate-x-1/2 *:-translate-y-1/2 *:left-1/2 *:top-1/2 *:w-[1.625rem] *:h-[1.625rem]">
+          <IconPost />
         </div>
-        <GeoData post={post} />
-        <ItemProfile user={user} provider={EnumTypeProvider.POST} targetId={id!} />
-      </section>
+        <h3 className="text-base font-semibold text-text-primary text-ellipsis line-clamp-2">{title ?? "Заголовок события"}</h3>
+      </header>
+      <p className="whitespace-pre-wrap text-text-primary text-sm font-normal line-clamp-4 text-ellipsis">{description}</p>
+      {replaceImage.length > 0 ? <ItemImages images={replaceImage} /> : null}
+      <div className="w-full flex flex-row items-center justify-start gap-2.5 *:h-[1.875rem] *:rounded-[0.9375rem]">
+        <div className="px-2.5 w-fit bg-[var(--card-bg-yellow)] py-[0.3125rem] gap-1 grid grid-cols-[1.25rem_minmax(0,1fr)] items-center">
+          <div className="w-5 h-5 relative p-2.5 *:absolute *:top-1/2 *:left-1/2 *:-translate-x-1/2 *:-translate-y-1/2 *:w-5 *:h-5 *:scale-90">
+            <IconNote />
+          </div>
+          <span className="text-text-primary text-xs font-medium whitespace-nowrap">
+            {notes?.length || 0} {nameTitle(notes?.length, EnumTypeProvider.NOTE)}
+          </span>
+        </div>
+        <CommentsCount id={id!} />
+      </div>
+      <GeoData post={post} />
+      <ItemProfile user={user} provider={EnumTypeProvider.POST} targetId={id!} />
     </article>
   )
 }
