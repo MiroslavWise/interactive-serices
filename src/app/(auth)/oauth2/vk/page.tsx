fc4ce872b819259530a741161c42d5064a1ebc20
Miroslav Wise <mipik1997@gmail.com>
2025-01-16 15:47:14 +0300

    a

--- src/app/(auth)/oauth2/vk/page.tsx
+++ src/app/(auth)/oauth2/vk/page.tsx
@@ -72,42 +72,43 @@ export default function CallbackVK() {
         fetchVK(data).then((response) => {
           if (response.ok) {
             if (response?.response) {
-              serviceAuth.postVK(response?.response).then((response) => {
-                console.log("response: postVK", response)
-                if (response.ok) {
-                  if (response?.res) {
-                    queryClient
-                      .fetchQuery({
-                        queryFn: () => getUserId(response.res?.id!),
-                        queryKey: ["user", { userId: response.res?.id }],
-                      })
-                      .then(({ data }) => {
-                        if (!!data) {
-                          // if (!data?.profile?.username) {
-                          //   dispatchOnboarding("open")
-                          // }
-                          dispatchAuthToken({ auth: response.res!, user: data! })
-                          handlePush("/")
-                          on({
-                            message: "Авторизация через сервис ВКонтакте прошла успешно",
-                          })
-                        } else {
-                          on({
-                            message:
-                              "К сожалению, сейчас мы не можем авторизовать вас через ВКонтакте. Пожалуйста, попробуйте другой способ.",
-                          })
-                          handlePush("/")
-                        }
-                      })
-                  }
-                } else {
-                  on({
-                    message:
-                      "У нас произошла какая-то ошибка, и мы не смогли вас авторизовать на сервисе. Возможно, ВКонтакте проводит какие-то операции, попробуйте чуть позже",
-                  })
-                  handlePush("/")
-                }
-              })
+              console.log("response: postVK", response)
+              // serviceAuth.postVK(response?.response).then((response) => {
+              //   console.log("response: postVK", response)
+              //   if (response.ok) {
+              //     if (response?.res) {
+              //       queryClient
+              //         .fetchQuery({
+              //           queryFn: () => getUserId(response.res?.id!),
+              //           queryKey: ["user", { userId: response.res?.id }],
+              //         })
+              //         .then(({ data }) => {
+              //           if (!!data) {
+              //             // if (!data?.profile?.username) {
+              //             //   dispatchOnboarding("open")
+              //             // }
+              //             dispatchAuthToken({ auth: response.res!, user: data! })
+              //             handlePush("/")
+              //             on({
+              //               message: "Авторизация через сервис ВКонтакте прошла успешно",
+              //             })
+              //           } else {
+              //             on({
+              //               message:
+              //                 "К сожалению, сейчас мы не можем авторизовать вас через ВКонтакте. Пожалуйста, попробуйте другой способ.",
+              //             })
+              //             handlePush("/")
+              //           }
+              //         })
+              //     }
+              //   } else {
+              //     on({
+              //       message:
+              //         "У нас произошла какая-то ошибка, и мы не смогли вас авторизовать на сервисе. Возможно, ВКонтакте проводит какие-то операции, попробуйте чуть позже",
+              //     })
+              //     handlePush("/")
+              //   }
+              // })
             }
           } else {
             console.log("error vk: ", response?.message)
