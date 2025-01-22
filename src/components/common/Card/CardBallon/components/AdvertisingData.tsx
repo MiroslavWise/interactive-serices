058deb1dccc4c8ae4b994afd2236b2388e8b4473
Miroslav Wise <mipik1997@gmail.com>
2025-01-22 12:39:48 +0300

    a

--- src/components/common/Card/CardBallon/components/AdvertisingData.tsx
+++ src/components/common/Card/CardBallon/components/AdvertisingData.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 
 function AdvertisingData({ company }: IProps) {
-  const { title: companyTitle, erid: companyErid, inn: companyInn } = company ?? {}
+  const { title: companyTitle, erid: companyErid, inn: companyInn, ogrn, ad } = company ?? {}
   const [isOpenCompany, setIsOpenCompany, refCompany] = useOutsideClickEvent()
 
   return !!company ? (
@@ -23,13 +23,17 @@ function AdvertisingData({ company }: IProps) {
       Реклама
       <div
         className={cx(
-          "py-3 px-2 flex flex-col gap-1 rounded-sm absolute z-20 bottom-full left-0 shadow-md min-w-32 max-w-fit bg-BG-second transition-all duration-200",
+          "py-3 px-2 flex flex-col gap-1 rounded-sm absolute z-20 bottom-full left-0 shadow-md w-fit min-w-40 max-w-48 bg-BG-second transition-all duration-200",
           isOpenCompany ? "opacity-100 visible" : "opacity-0 invisible",
         )}
       >
         <span className="text-text-primary text-xs font-medium">{companyTitle}</span>
         <span className="text-text-primary text-xs font-medium whitespace-nowrap">ИНН: {companyInn}</span>
         <span className="text-text-primary text-xs font-normal whitespace-nowrap">erid: {companyErid}</span>
+        <span className="text-text-primary text-xs font-normal whitespace-nowrap">erid: {companyErid}</span>
+        <span className={cx("text-text-primary text-xs font-normal whitespace-nowrap", !ogrn && "hidden")}>ОРГН: {ogrn}</span>
+        <br className={cx(!ad && "hidden")} />
+        <span className={cx("text-text-secondary text-xs font-normal", !ad && "hidden")}>{ad}</span>
       </div>
     </span>
   ) : null
