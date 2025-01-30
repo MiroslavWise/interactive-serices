728197bf3254475feebc149e48f69fbb405f5c69
Miroslav Wise <mipik1997@gmail.com>
2025-01-30 12:51:17 +0300

    a

--- src/helpers/user-interlocutor.ts
+++ src/helpers/user-interlocutor.ts
@@ -29,7 +29,7 @@ function typeMessage({
   isGroup?: boolean
 }): string | null {
   if (isGroup) return `Групповой${offer ? `: ${offer?.title ?? ""}` : ""}`
-  if (provider === EnumProviderThreads.HELP) return `Помощь${offer ? `: ${offer?.title ?? ""}` : ""}`
+  if (provider === EnumProviderThreads.HELP) return `Умение или услуга${offer ? `: ${offer?.title ?? ""}` : ""}`
   if (provider === EnumProviderThreads.PERSONAL) return "Личные"
   if (provider === EnumProviderThreads.BARTER && !!offer)
     return `${objProvider[EnumProviderThreads.BARTER]} ${offer?.category?.title || "Предложение"}`
