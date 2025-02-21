ff051747259d44b7aeeba08d68e5649f8c4e81c7
Miroslav Wise <mipik1997@gmail.com>
2025-02-21 13:04:20 +0300

    a

--- src/components/templates/CreateNewNote/schema.ts
+++ src/components/templates/CreateNewNote/schema.ts
@@ -4,7 +4,7 @@ import { arktypeResolver } from "@hookform/resolvers/arktype"
 import { MAX_LENGTH_DESCRIPTION_NOTE } from "@/config/constants"
 
 const schema = type({
-  description: `0 < string <= ${MAX_LENGTH_DESCRIPTION_NOTE} = ""`,
+  description: `0 < string <= ${MAX_LENGTH_DESCRIPTION_NOTE}`,
   is: "boolean",
   file: type({
     file: type.instanceOf(File).array(),
